fc24ecbdce84ea365de7797454110cf037def55f
gil7788 <gil7788@gmail.com>
2024-01-27 10:51:49 +0200

    Refactor Source Code to TypeScript

--- test/testSetup.js
+++
@@ -1,8 +0,0 @@
-const { generateData } = require('./integration/populate_db');
-
-async function setup() {
-    await generateData();
-    console.log('Test database has been populated.');
-}
-
-setup();
